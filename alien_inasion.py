513980ea45c94c01a374418a45504113329f098a
Shahzadforyou <zaeimshahzad@gmail.com>
2026-03-19 15:00:08 +0500

    Add background color and screen.fill()
    Introduce a bg_color tuple and call screen.fill(bg_color) each loop to clear the screen before drawing. Adds a brief comment for the background color and adjusts indentation/comments around the game initialization and main loop.

--- alien_inasion.py
+++ alien_inasion.py
@@ -1,11 +1,14 @@
 import sys
 import pygame
+from setting import Setting
 
 def run_game():
-    pygame.init()              
+    pygame.init()   
+    ai_settings = Setting()           
                                                                             #initilizing gaming environment and screen
-    screen = pygame.display.set_mode((1200,800))
+    screen = pygame.display.set_mode(ai_settings.width,ai_settings.length)
     pygame.display.set_caption("Alien Invasion")
+        #settin background color
                                                                            #main loop for game
     while True:
                                                                            #checking for event from screen
@@ -14,5 +17,6 @@ def run_game():
                 sys.exit()
 
 
+        screen.fill(ai_settings.bg_color)
         pygame.display.flip()
 run_game()
---
+++ setting.py
@@ -0,0 +1,6 @@
+class Setting():
+    #A class to Store seeting for game
+    def __init__(self):
+        self.width = 1200
+        self.length = 800
+        self.bg_color = (230,230,230)
